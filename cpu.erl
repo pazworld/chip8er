3ff4bc4e3fd4fa3defec231900f576a1629c5ccf
pazworld <paz@moudamepo.net>
2020-07-27 01:43:35 +0900

    rewrite alu with record regs

--- cpu.erl
+++ cpu.erl
@@ -1,15 +1,24 @@
 -module(cpu).
--export([alu/1]).
+-export([alu/2]).
 
-alu(<<3:4, _Operand:12>>) ->
-    16#201;
+-record(regs, {pc, i, v={0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}).
 
-alu(<<16#A:4, Operand:12>>) ->
-    {ok, Operand};
+alu(<<3:4, Reg:4, Operand:8>>, R) ->
+    PC = R#regs.pc,
+    VR = element(Reg + 1, R#regs.v),
+    case VR of
+        Operand -> {ok, PC + 1};
+        _       -> {ok, PC}
+    end;
 
-alu(<<16#C:4, _Reg:4, Mask:8>>) ->
+alu(<<16#A:4, Operand:12>>, R) ->
+    {ok, R#regs{i=Operand}};
+
+alu(<<16#C:4, Reg:4, Mask:8>>, R) ->
     Random = (rand:uniform(256) - 1) band Mask,
-    {ok, {0, Random}};
+    V = R#regs.v,
+    V2 = setelement(Reg + 1, V, Random),
+    {ok, R#regs{v=V2}};
 
-alu(_) ->
+alu(_, _) ->
     unimplemented.
--- cpu_tests.erl
+++ cpu_tests.erl
@@ -1,17 +1,21 @@
 -module(cpu_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-record(regs, {pc, i, v={0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}).
+
 return_unimplemented_with_unimplemented_opcode_test() ->
-    ?assertEqual(unimplemented, cpu:alu(0)).
+    ?assertEqual(unimplemented, cpu:alu(0, #regs{})).
 
 a21e_set_21e_to_i_register_test() ->
-    {_, I} = cpu:alu(<<16#A21E:16>>),
+    {_, R} = cpu:alu(<<16#A21E:16>>, #regs{i=0}),
+    I = R#regs.i,
     ?assertEqual(16#21E, I).
 
 c201_set_0_or_1_to_v2_register_test() ->
-    {_, {_, V2}} = cpu:alu(<<16#C201:16>>),
+    {_, R} = cpu:alu(<<16#C201:16>>, #regs{}),
+    V2 = element(2 + 1, R#regs.v),
     ?assert(V2 =:= 0 orelse V2 =:= 1).
 
 op3201_skip_next_if_v2_register_is_01_test() ->
-    PC = cpu:alu(<<16#3201:16>>),
-    ?assertEqual(16#201, PC).
+    {_, PC} = cpu:alu(<<16#3201:16>>, #regs{pc=16#300, v={0, 0, 1}}),
+    ?assertEqual(16#301, PC).
